b016cfc9162c8b6c657f40a63669585aba5afea7
BoBo <fekbox@outlook.com>
2025-08-25 18:44:04 +0800

    feat: Add layers config.

---
+++ layers/ui/components/Box.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div>
+    Box component from ui layer {{ props.title }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
